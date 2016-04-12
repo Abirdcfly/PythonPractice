6ef797f0c2e98f89ad6de9e529449c05d7d83372
abirdcfly <fp544037857@gmail.com>
2016-04-13 01:44:35 +0800

    = =

--- test.py
+++ test.py
@@ -279,3 +279,323 @@
 # context = defaults.copy()
 # context.update(user)
 # print user, '\n', defaults, '\n', context
+
+# compile('hahha')
+
+# from decimal import *
+
+# a = [1,2,3]
+# print id(a)
+# a.insert(0,8)
+# # a[0:0] = [8]
+# print a,id(a)
+#
+# b = 2
+#
+# print type(a),type(b)
+# print type(a) == type(b)
+# print type(a) is type(b)
+#
+# print dir( )
+#
+# a = 10
+# b = 10
+# c = 1000
+# d = 1000
+# e = 10.0
+# f = 10.0
+# print a is b
+# print c is d
+# print e is f
+
+# print dir().getfile()
+
+# for i in  range(2):
+#     print i
+#
+# for i in range(4,6):
+#     print i
+
+# class Person:
+#     def __init__(self):
+#         pass
+#
+#     def getAge(self):
+#         print __name__
+#
+# p = Person()
+# p.getAge()
+
+# names1 = ['Amir', 'Barry', 'Chales', 'Dao']
+#
+# if 'amir' in names1:
+#     print 1
+# else:
+#     print 2
+
+# numbers = [1, 2, 3, 4]
+#
+# numbers.append([5,6,7,8])
+#
+# print len(numbers)
+# print numbers
+
+# kvps = { '1' : 1, '2' : 2 }
+# theCopy = kvps.copy()
+#
+# kvps['1'] = 5
+#
+# sum = kvps['1'] + theCopy['1']
+# print sum
+
+# a = []
+# import fileinput
+# for index, line in enumerate(fileinput.input()):
+#     a[index] = line
+# print pow(a[0], a[1])
+# print pow(a[0], a[1], a[2])
+
+
+# a = int(raw_input())
+# b = int(raw_input())
+# c = int(raw_input())
+# print pow(a, b)
+# print pow(a, b, c)
+
+# from collections import Counter
+#
+# data = [0, 2, 4, 5, 2, 2, 2]
+#
+# for key, value in Counter(data).items():
+#     print key, value
+#     # if value > (len(data) / 2):
+#     #     print(key)
+#
+# # Out: 2
+#
+# a = {1:2,2:3,3:4}
+# print a.items()
+#
+# x = {'a': 1, 'b': 2}
+# y = {'b': 3, 'c': 4}
+# z = {k: v for d in (x,y) for k, v in d.items()}
+# print z
+# from pprint import pprint
+# mat = [[1,2,3], [4,5,6], [7,8,9]]
+# new_mat = [ [row[i] for row in mat] for i in [0,1,2] ] # 嵌套
+# pprint(new_mat)  # [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
+# import random
+# def random_pick(seq, probabilities):
+#     x = random.uniform(0, 1)
+#     cumulative_probability = 0.0
+#     for item, item_probability in zip(seq, probabilities):
+#             cumulative_probability += item_probability
+#             if x < cumulative_probability:
+#                 break
+#     return item
+#
+#
+# for i in range(15):
+#     print random_pick("abc",[0.1,0.3,0.6])
+
+# a = [1,2,3]
+# b = list(a)
+# print id(a), id(b)  # 140298869519856 140298869576192
+# for x, y in zip(a, b):       # 子对象身份相同
+#     print(id(x), id(y))
+#     # (34189656, 34189656)
+#     # (34189632, 34189632)
+#     # (34189608, 34189608)
+
+# import copy
+# a = [1, 2, 3]
+# b = copy.deepcopy(a)
+# print id(a), id(b)  # 140298869557512 140298869519856
+# for x, y in zip(a, b):       # 子对象身份相同,2333 因为1,2,3是不可变对象
+#     print(id(x), id(y))
+#     # (34189656, 34189656)
+#     # (34189632, 34189632)
+#     # (34189608, 34189608)
+#
+#
+# import copy
+# a = [[1],[2],[3]]
+# b = copy.deepcopy(a)
+# print id(a), id(b)  # 139930602280504 139930602280288
+# for x, y in zip(a, b):       # 子对象身份不同
+#     print(id(x), id(y))
+#     # (139930602147208, 139930602280576)
+#     # (139930602146056, 139930602280720)
+#     # (139930602280216, 139930602280792)
+#
+# def makeHtmlTag(tag, *args, **kwds):
+#     def real_decorator(fn):
+#         css_class = " class='{0}'".format(kwds["css_class"]) \
+#                                      if "css_class" in kwds else ""
+#         def wrapped(*args, **kwds):
+#             return "<"+tag+css_class+">" + fn(*args, **kwds) + "</"+tag+">"
+#         return wrapped
+#     return real_decorator
+#
+# @makeHtmlTag(tag="b", css_class="bold_css")
+# @makeHtmlTag(tag="i", css_class="italic_css")
+# def hello():
+#     return "hello world"
+#
+# print hello()
+# print hello.__name__
+
+# class myDecorator(object):
+#
+#     def __init__(self, fn):
+#         print "inside myDecorator.__init__()"
+#         self.fn = fn
+#
+#     def __call__(self):
+#         self.fn()
+#         print "inside myDecorator.__call__()"
+#
+# @myDecorator
+# def aFunction():
+#     print "inside aFunction()"
+#
+# print "Finished decorating aFunction()"
+#
+# aFunction()
+#
+# # 输出：
+# # inside myDecorator.__init__()
+# # Finished decorating aFunction()
+# # inside aFunction()
+# # inside myDecorator.__call__()
+#
+# from functools import wraps
+# def memo(fn):
+#     cache = {}
+#     # miss = object()
+#     miss = 'dd'
+#
+#     @wraps(fn)
+#     def wrapper(*args):
+#         # result = cache.get(args)
+#         result = cache.get(args, None)
+#         if result is None:
+#             result = fn(*args)
+#             cache[args] = result
+#         return result
+#
+#     return wrapper
+#
+# @memo
+# def fib(n):
+#     if n < 2:
+#         return n
+#     return fib(n - 1) + fib(n - 2)
+#
+# import time
+# a = time.time()
+# print fib(1000)
+# print time.time() - a
+
+# import time
+# a = time.time()
+#
+# def test(x):
+#     a, b, i = 0, 1, 0
+#     while i < x:
+#         yield b
+#         a, b = b, a+b
+#         i += 1
+#
+# for i in test(10000):
+#     pass
+# print i
+# print time.time() - a
+#
+# from threading import Thread
+# from functools import wraps
+#
+# def async(func):
+#     @wraps(func)
+#     def async_func(*args, **kwargs):
+#         func_hl = Thread(target = func, args = args, kwargs = kwargs)
+#         func_hl.start()
+#         return func_hl
+#
+#     return async_func
+#
+# if __name__ == '__main__':
+#     from time import sleep
+#
+#     @async
+#     def print_somedata():
+#         print 'starting print_somedata'
+#         sleep(2)
+#         print 'print_somedata: 2 sec passed'
+#         sleep(2)
+#         print 'print_somedata: 2 sec passed'
+#         sleep(2)
+#         print 'finished print_somedata'
+#
+#     def main():
+#         print_somedata()
+#         print 'back in main'
+#         print_somedata()
+#         print 'back in main'
+#
+#     main()
+
+import time
+from functools import wraps
+# def logger(fn):
+#     @wraps(fn)
+#     def wrapper(*args, **kwargs):
+#         ts = time.time()
+#         result = fn(*args, **kwargs)
+#         te = time.time()
+#         print "function      = {0}".format(fn.__name__)
+#         print "    arguments = {0} {1}".format(args, kwargs)
+#         print "    return    = {0}".format(result)
+#         print "    time      = %.6f sec" % (te-ts)
+#         return result
+#     return wrapper
+
+
+# import inspect
+# def get_line_number():
+#     return inspect.currentframe().f_back.f_back.f_lineno
+#
+# def logger(loglevel):
+#     def log_decorator(fn):
+#         @wraps(fn)
+#         def wrapper(*args, **kwargs):
+#             ts = time.time()
+#             result = fn(*args, **kwargs)
+#             te = time.time()
+#             print "function   = " + fn.__name__,
+#             print "    arguments = {0} {1}".format(args, kwargs)
+#             print "    return    = {0}".format(result)
+#             print "    time      = %.6f sec" % (te-ts)
+#             if (loglevel == 'debug'):
+#                 print "    called_from_line : " + str(get_line_number())
+#             return result
+#         return wrapper
+#     return log_decorator
+#
+#
+#
+# @logger
+# def multipy(x, y):
+#     return x * y
+#
+# @logger
+# def sum_num(n):
+#     s = 0
+#     for i in xrange(n+1):
+#         s += i
+#     return s
+#
+# print multipy(2, 10)
+# # print sum_num(100)
+# # print sum_num(10000000)
